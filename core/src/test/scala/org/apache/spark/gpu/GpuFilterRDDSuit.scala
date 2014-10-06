d14e4ea1a8d0912c806d36ccb2376f6fff5177bf
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-10-06 13:23:19 -0400

    Skeleton for GPU filter testcases

---
+++ core/src/test/scala/org/apache/spark/gpu/GpuFilterRDDSuit.scala
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.gpu
+
+import org.apache.spark.SharedSparkContext
+import org.apache.spark.deploy.worker.WorkerArguments
+import org.apache.spark.rdd.{ChunkIterator, RDDChunk}
+import org.scalatest.FunSuite
+
+import scala.collection.immutable.IndexedSeq
+import scala.language.existentials
+import scala.reflect.ClassTag
+
+/**
+ *
+ */
+class GpuFilterRDDSuit extends FunSuite with SharedSparkContext {
+
+  test("org.apache.spark.rdd.GpuRDD.filter test") {
+    val testData: IndexedSeq[(Int, Int)] = (0 to 10).reverse.zipWithIndex
+
+    val rdd = sc.parallelize(testData)
+    val gpuRdd = rdd.toGpuFilterRDD(Array("INT", "INT"), 1, "==", 1)
+    val collectedChunks = gpuRdd.collect()
+    assert(collectedChunks.length === 1)
+    val chunk = collectedChunks(0)
+    (0 until chunk.MAX_SIZE).foreach(i =>
+      if (i <= 10) {
+        assert(chunk.intData(0)(i) === (10 - i), "values do not match")
+        assert(chunk.intData(1)(i) === i, "indexes  do not match")
+      } else {
+        assert(chunk.intData(0)(i) === 0, "values do not match")
+        assert(chunk.intData(1)(i) === 0, "values do not match")
+      }
+    )
+  }
+
+}
