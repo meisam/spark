9ce3ef1b43d2c2f9d22c2fa5847c140bf977f631
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-10-09 07:52:09 -0400

    Fixing GPU prefix sum

--- core/src/main/scala/org/apache/spark/rdd/GpuFilteredRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuFilteredRDD.scala
@@ -329,30 +329,31 @@ class FilteredChunkIterator[T <: Product]
 
   def prefixSum(counts: Array[Int], prefixSums: Array[Int]): Unit = {
 
-    // using double buffers to avoid copying data
-    val buffer1 = createReadWriteBuffer(Sizeof.cl_int * counts.length)
-    val buffer2 = createReadWriteBuffer(Sizeof.cl_int * counts.length)
+    if (counts.length != prefixSums.length) {
+      throw new IllegalArgumentException("Input and output arrays should have the same size (%d != %d)".format(counts.length, prefixSums.length))
+    }
 
+    val globalSize = POW_2_S.filter(_ >= counts.length).head
+    val localSize = Math.min(globalSize, BLOCK_SIZE)
+    val global_work_size = Array[Long](globalSize)
+    val local_work_size = Array[Long](localSize)
+
+    // using double buffers to avoid copying data
+    val buffer1 = createReadWriteBuffer(Sizeof.cl_int * globalSize)
+    val buffer2 = createReadWriteBuffer(Sizeof.cl_int * globalSize)
     hostToDeviceCopy(Pointer.to(counts), buffer1, Sizeof.cl_int * counts.length)
     val kernel = clCreateKernel(openCLContext.getOpenCLProgram, "prefix_sum_stage", null)
     var stride: Int = 0
-    var switchedBuffers = true
-    val globalSize = POW_2_S.filter(_ >= counts.length).head
 
-    val localSize = Math.min(globalSize, BLOCK_SIZE)
+    var switchedBuffers = true
 
     while (stride <= counts.length) {
       clSetKernelArg(kernel, if (switchedBuffers) 0 else 1, Sizeof.cl_mem, Pointer.to(buffer1))
       clSetKernelArg(kernel, if (switchedBuffers) 1 else 0, Sizeof.cl_mem, Pointer.to(buffer2))
       clSetKernelArg(kernel, 2, Sizeof.cl_int, Pointer.to(Array[Int](stride)))
-      val global_work_size = Array[Long](globalSize)
-      val local_work_size = Array[Long](localSize)
       clEnqueueNDRangeKernel(openCLContext.getOpenCLQueue, kernel, 1, null, global_work_size, local_work_size, 0, null, null)
       switchedBuffers = !switchedBuffers
       stride = if (stride == 0) 1 else stride << 1
-    }
-    if (counts.length != prefixSums.length) {
-      throw new IllegalArgumentException("Input and output arrays should have the same size (%d != %d)".format(counts.length, prefixSums.length))
     }
     val results = if (switchedBuffers) buffer1 else buffer2
     deviceToHostCopy(results, Pointer.to(prefixSums), Sizeof.cl_int * globalSize)
