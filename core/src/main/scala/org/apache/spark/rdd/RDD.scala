b84901499e3e85149b2a6c52693310bc6ac87ddb
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-10-22 16:29:06 -0400

    Cosmetivcs: removing extra semicolon

--- core/src/main/scala/org/apache/spark/rdd/RDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/RDD.scala
@@ -1360,7 +1360,7 @@ abstract class RDD[T: ClassTag](
   }
 
   def toGpuRDD(columnTypes: Array[String]) = {
-    new GpuRDD(this.asInstanceOf[RDD[Product]], columnTypes);
+    new GpuRDD(this.asInstanceOf[RDD[Product]], columnTypes)
   }
 
   def toGpuFilterRDD(columnTypes: Array[String], columnIndex: Int, operation: Int, value: Int) = {
