afd0f48c75bc09c436002730b19b1d9e3f96ee8a
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-10-24 12:44:03 -0400

    [Unstable] organizing imports after moving classes and renaming them
    #14: Refactor GpuFilteredRDD and FilteredChunkItrator 
    
    Task-Url: http://github.com/meisam/spark/issues/issue/14

--- core/src/main/scala/org/apache/spark/rdd/GpuFilteredRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuFilteredRDD.scala
@@ -1,11 +1,7 @@
 package org.apache.spark.rdd
 
-import org.apache.spark.scheduler.OpenCLContext
 import org.apache.spark.{Partition, TaskContext}
-import org.jocl.CL._
-import org.jocl._
 
-import scala.collection.immutable.IndexedSeq
 import scala.reflect.ClassTag
 
 /**
