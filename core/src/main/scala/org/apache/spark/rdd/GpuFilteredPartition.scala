c02ce55103c0d39199232d58489e4798a84b8fd1
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-10-29 22:02:46 -0400

    rewriting gpu filtered partition
    #14: Refactor GpuFilteredRDD and FilteredChunkItrator
    
    Task-Url: http://github.com/meisam/spark/issues/issue/14

--- core/src/main/scala/org/apache/spark/rdd/GpuFilteredPartition.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuFilteredPartition.scala
@@ -3,29 +3,28 @@ package org.apache.spark.rdd
 import org.apache.spark.scheduler.OpenCLContext
 
 import scala.reflect.ClassTag
+import scala.reflect.runtime.universe.TypeTag
 
-class GpuFilteredPartition[T <: Product : ClassTag]
-(context: OpenCLContext, columnTypes: Array[String], colIndex: Int, operation: Int, value: Int,
- capacity: Int) extends GpuPartition[T](context, columnTypes, capacity) {
+class GpuFilteredPartition[T <: Product : ClassTag, U: ClassTag : TypeTag]
+(context: OpenCLContext, columnTypes: Array[String], colIndex: Int,
+ operation: ComparisonOperation.Value, value: U, capacity: Int)
+  extends GpuPartition[T](context, columnTypes, capacity) {
+
   override def fill(iter: Iterator[T]): Unit = {
     val startTransformDataTime = System.nanoTime
+    super.fill(iter)
     val endTransformDataTime = System.nanoTime
-    super.fill(iter: Iterator[T])
     val startSelectionTotalTime = System.nanoTime
+    val resultSize = filter[U](colIndex, value, operation)
 
-    if (columnTypes(colIndex) == "INT") {
-      val resultSize = compute(intData(colIndex), size.toLong, value, operation)
-
-      size = resultSize
-      intData.zipWithIndex.filter(_._1 != null).foreach({
-        case (inData: Array[Int], index) => {
-          if (index != colIndex) {
-            project(intData(colIndex), size, intData(colIndex), resultSize)
-          }
+    intData.zipWithIndex.filter(_._1 != null).foreach({
+      case (inData: Array[Int], index) => {
+        if (index != colIndex) {
+          project(intData(colIndex), size, intData(colIndex), resultSize)
         }
-      })
-      size = resultSize
-    }
+      }
+    })
+    size = resultSize
     val endSelectionTotalTime = System.nanoTime
 
     val totalTime = endSelectionTotalTime - startTransformDataTime
