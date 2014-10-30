9b4a6a91611a4291e80d624b848511419cd3d245
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-10-30 18:38:24 -0400

    implementing GpuJoinPartition#buildHashTable
    #24: Port hash join from opencl/cuda to Scala/Java
    
    Task-Url: http://github.com/meisam/spark/issues/issue/24

--- core/src/main/scala/org/apache/spark/rdd/GpuJoinPartition.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuJoinPartition.scala
@@ -14,6 +14,77 @@ U: ClassTag : TypeTag]
   extends GpuPartition[T](context, columnTypes, capacity) {
 
   def buildHashTable() = {
+
+    val hsize = (1 to 31).map(1 << _).filter(_ >= rightPartition.size).head
+
+    // Number of primary keys for each hash value
+    val gpu_hashNum = createReadWriteBuffer[Int](hsize)
+
+    val memSetKernel = clCreateKernel(context.program, "cl_memset_int", null)
+    clSetKernelArg(memSetKernel, 0, Sizeof.cl_mem, Pointer.to(gpu_hashNum))
+    clSetKernelArg(memSetKernel, 1, Sizeof.cl_int, pointer(Array(hsize)))
+
+    val global_work_size = Array[Long](globalSize)
+    val local_work_size = Array[Long](localSize)
+
+    clEnqueueNDRangeKernel(context.getOpenCLQueue, memSetKernel, 1, null, global_work_size, local_work_size, 0,
+      null, null)
+
+    val threadNum = globalSize
+
+    // Per thread
+    val gpu_count = createReadWriteBuffer[Int](threadNum)
+    // Per thread move to join on GPU
+    val gpu_resPsum = createReadWriteBuffer[Int](threadNum)
+
+    // prefix sum for gpu_hashNum
+    val gpu_psum = createReadWriteBuffer[Int](hsize)
+
+    //twice as big as # of keys
+    val gpu_bucket = createReadWriteBuffer[Int](2 * rightPartition.size) //TODO change [Int[ to [U]
+
+    val gpu_psum1 = createReadWriteBuffer[Int](hsize)
+
+    val columnPosition = rightPartition.dataPosition(joinColIndexRight)
+
+    val gpu_dim = columnPosition match {
+      case DataPosition.HOST =>
+        createReadBuffer[Int](rightPartition.size) // TODO should this be [Int] or [U]
+      case DataPosition.DEVICE =>
+        throw new NotImplementedError("DataPosition.DEVICE is not supported!")
+      case _ =>
+        throw new NotImplementedError("Unknown DataPosition type!")
+
+    }
+    val rightColumn: Array[U] = rightPartition.getColumn[U](joinColIndexRight)
+    //TODO why this should be blocking
+    hostToDeviceCopy[U](pointer(rightColumn), gpu_dim, /*CL_TRUE,*/ rightPartition.size)
+
+    val countHashKernel = clCreateKernel(context.program, "count_hash_num", null)
+    clSetKernelArg(countHashKernel, 0, Sizeof.cl_mem, Pointer.to(gpu_dim))
+    clSetKernelArg(countHashKernel, 1, Sizeof.cl_long,
+      pointer(Array[Long](rightPartition.size.toLong)))
+    clSetKernelArg(countHashKernel, 2, Sizeof.cl_mem, Pointer.to(gpu_hashNum))
+    clSetKernelArg(countHashKernel, 3, Sizeof.cl_int, pointer(Array[Long](hsize)))
+    clEnqueueNDRangeKernel(context.queue, countHashKernel, 1, null, global_work_size,
+      local_work_size, 0, null, null)
+    scanImpl(gpu_hashNum, hsize, gpu_psum)
+
+    deviceToDeviceCopy[Int](gpu_psum, gpu_psum1, hsize)
+
+    val buildHashTableKernel = clCreateKernel(context.program, "build_hash_table", null)
+    clSetKernelArg(buildHashTableKernel, 0, Sizeof.cl_mem, Pointer.to(gpu_dim))
+    clSetKernelArg(buildHashTableKernel, 1, Sizeof.cl_long, pointer(Array[Long](rightPartition.size.toLong)))
+    clSetKernelArg(buildHashTableKernel, 2, Sizeof.cl_mem, Pointer.to(gpu_psum1))
+    clSetKernelArg(buildHashTableKernel, 3, Sizeof.cl_mem, Pointer.to(gpu_bucket))
+    clSetKernelArg(buildHashTableKernel, 4, Sizeof.cl_int, pointer(Array[Int](hsize)))
+    clEnqueueNDRangeKernel(context.queue, buildHashTableKernel, 1, null, global_work_size,
+      local_work_size, 0, null, null)
+
+    if (columnPosition == DataPosition.HOST)
+      clReleaseMemObject(gpu_dim)
+
+    clReleaseMemObject(gpu_psum1)
   }
 
   def joinOnGpu(): Int = {
