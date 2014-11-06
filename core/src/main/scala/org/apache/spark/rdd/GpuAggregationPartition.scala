371b90ee2ef638a46ab790005f43ee02c1479a75
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-11-06 18:43:21 -0500

    Cosmetics and formatting

--- core/src/main/scala/org/apache/spark/rdd/GpuAggregationPartition.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuAggregationPartition.scala
@@ -69,7 +69,8 @@ class GpuAggregationPartition[T <: Product : TypeTag]
     hostToDeviceCopy[Int](pointer(cpuOffsets), gpuOffsets, cpuOffsets.length)
 
     val gpuGbType = createReadBuffer[Int](groupByColumnIndexes.length)
-    hostToDeviceCopy[Int](pointer(groupByColumnIndexes), gpuGbType, groupByColumnIndexes.length)
+    hostToDeviceCopy[Int](pointer(groupByColumnIndexes), gpuGbType, groupByColumnIndexes
+      .length)
 
     val gpuGbSize = createReadBuffer[Int](groupByColumnIndexes.length)
     val groupBySize: Array[Int] = groupByColumnIndexes.map(columnTypes(_)).map(baseSize(_))
