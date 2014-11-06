ab2d5188c0dd50f1dc3788f0ed7d60f3091f8adf
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-11-06 17:33:38 -0500

    Removing explicit column types from GpuSortPartition
    #33: Use TypeTags to infer type of GpuRDDs instead of passing column types as an argument
    
    Task-Url: http://github.com/meisam/spark/issues/issue/33

--- core/src/main/scala/org/apache/spark/rdd/GpuSortPartition.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuSortPartition.scala
@@ -2,13 +2,12 @@ package org.apache.spark.rdd
 
 import org.apache.spark.scheduler.OpenCLContext
 
-import scala.reflect.ClassTag
-;
+import scala.reflect.runtime.universe.TypeTag
 
-class GpuSortPartition[T <: Product : ClassTag]
-(context: OpenCLContext, columnTypes: Array[String], groupByColumnIndexes: Array[Int],
+class GpuSortPartition[T <: Product : TypeTag]
+(context: OpenCLContext, groupByColumnIndexes: Array[Int],
  aggregations: Array[AggregationOperation.Value], capacity: Int)
-  extends GpuPartition[T](context, columnTypes, capacity) {
+  extends GpuPartition[T](context, capacity) {
 
   def sort(): Unit = {
 
