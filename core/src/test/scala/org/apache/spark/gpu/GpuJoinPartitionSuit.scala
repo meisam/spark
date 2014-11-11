7eb0b96772d015e56ff320e28bae5c87a9876ed3
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-11-11 14:21:35 -0500

    Updating test cases for join to match the new design of GpuJoinPartition
    Task-Url: http://github.com/meisam/spark/issues/issue/26

--- core/src/test/scala/org/apache/spark/gpu/GpuJoinPartitionSuit.scala
+++ core/src/test/scala/org/apache/spark/gpu/GpuJoinPartitionSuit.scala
@@ -39,27 +39,77 @@ class GpuJoinPartitionSuit extends FunSuite with SharedSparkContext {
   }
 
   test("GpuJoinPartition(Int, Int) == 1 match test") {
-    val testData: IndexedSeq[(Int, Int)] = Array(10, 11, 10, 11, 12, 13).zipWithIndex
+    val leftTableData: IndexedSeq[(Int, Int)] = Array(10, 11, 10, 11, 12, 13, 13).zipWithIndex
+    val leftPartition = new GpuPartition[(Int, Int)](openCLContext, DEFAULT_CAPACITY)
+    leftPartition.fill(leftTableData.toIterator)
 
     val rightTableData = Array(9, 10, 12, 13, 15).zipWithIndex
-
-    val gpuPartition = new GpuPartition[(Int, Int)](openCLContext,
-      DEFAULT_CAPACITY)
-    gpuPartition.fill(rightTableData.toIterator)
+    val rightPartition = new GpuPartition[(Int, Int)](openCLContext, DEFAULT_CAPACITY)
+    rightPartition.fill(rightTableData.toIterator)
 
 
-    val gpuJoinPartition = new GpuJoinPartition[(Int, Int), (Int, Int), Int](openCLContext,
-      gpuPartition, 0, 0, DEFAULT_CAPACITY)
+    val gpuJoinPartition = new GpuJoinPartition[(Int, Int, Int), (Int, Int), (Int, Int), Int](
+      openCLContext, leftPartition, rightPartition, 0, 0, DEFAULT_CAPACITY)
+    gpuJoinPartition.join
 
-    gpuJoinPartition.fill(testData.toIterator)
     val expectedData = Array((10, 0, 1), (10, 2, 1), (12, 4, 2))
 
-    assert(gpuPartition.size === expectedData.length)
+    assert(rightPartition.size === expectedData.length)
 
     expectedData.zipWithIndex.foreach { case ((keyValue, leftValue, rightValue), index) =>
-      assert(gpuPartition.intData(0)(index) === keyValue, "values do not match")
-      assert(gpuPartition.intData(1)(index) === leftValue, "values do not match")
-      assert(gpuPartition.intData(2)(index) === rightValue, "values do not match")
+      assert(rightPartition.intData(0)(index) === keyValue, "values do not match")
+      assert(rightPartition.intData(1)(index) === leftValue, "values do not match")
+      assert(rightPartition.intData(2)(index) === rightValue, "values do not match")
+    case _ => fail("We should not be here")
+    }
+  }
+
+  test("GpuJoinPartition(Int, Int) basic test") {
+    val leftTableData = Array(12, 12, 13, 13, 14, 14, 15, 15, 16, 18, 18).zipWithIndex
+    val leftPartition = new GpuPartition[(Int, Int)](openCLContext, DEFAULT_CAPACITY)
+    leftPartition.fill(leftTableData.toIterator)
+
+    val rightTableData = Array(15, 16, 17).zipWithIndex
+    val rightPartition = new GpuPartition[(Int, Int)](openCLContext, DEFAULT_CAPACITY)
+    rightPartition.fill(rightTableData.toIterator)
+
+    val gpuJoinPartition = new GpuJoinPartition[(Int, Int, Int), (Int, Int), (Int, Int), Int](
+      openCLContext, leftPartition, rightPartition, 0, 0, DEFAULT_CAPACITY)
+    gpuJoinPartition.join
+
+    val expectedData = Array((10, 0, 1), (10, 2, 1), (12, 4, 2))
+
+    assert(rightPartition.size === expectedData.length)
+
+    expectedData.zipWithIndex.foreach { case ((keyValue, leftValue, rightValue), index) =>
+      assert(rightPartition.intData(0)(index) === keyValue, "values do not match")
+      assert(rightPartition.intData(1)(index) === leftValue, "values do not match")
+      assert(rightPartition.intData(2)(index) === rightValue, "values do not match")
+    case _ => fail("We should not be here")
+    }
+  }
+
+  test("GpuJoinPartition(Long, Int) basic test") {
+    val leftTableData: IndexedSeq[(Long, Int)] = Array(10L, 11L, 12L).zipWithIndex
+    val leftPartition = new GpuPartition[(Long, Int)](openCLContext, DEFAULT_CAPACITY)
+    leftPartition.fill(leftTableData.toIterator)
+
+    val rightTableData = Array(10L, 11L, 12L).zipWithIndex
+    val rightPartition = new GpuPartition[(Long, Int)](openCLContext, DEFAULT_CAPACITY)
+    rightPartition.fill(rightTableData.toIterator)
+
+    val gpuJoinPartition = new GpuJoinPartition[(Long, Int, Int), (Long, Int), (Long, Int), Long](
+      openCLContext, leftPartition, rightPartition, 0, 0, DEFAULT_CAPACITY)
+    gpuJoinPartition.join
+
+    val expectedData = Array((10L, 0, 1), (10L, 2, 1), (12L, 4, 2))
+
+    assert(rightPartition.size === expectedData.length)
+
+    expectedData.zipWithIndex.foreach { case ((keyValue, leftValue, rightValue), index) =>
+      assert(rightPartition.intData(0)(index) === keyValue, "values do not match")
+      assert(rightPartition.intData(1)(index) === leftValue, "values do not match")
+      assert(rightPartition.intData(2)(index) === rightValue, "values do not match")
     case _ => fail("We should not be here")
     }
   }
