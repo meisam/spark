d10cdb2efc9f43d3395379b618e84a4646596114
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-11-25 09:46:51 -0500

    [WIP] Fixing bugs in aggregation functions
    #30: port aggregations from opencl/cuda implementation
    
    Task-Url: http://github.com/meisam/spark/issues/issue/30

--- core/src/main/scala/org/apache/spark/rdd/GpuAggregationPartition.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuAggregationPartition.scala
@@ -7,62 +7,45 @@ import org.jocl.{ Pointer, Sizeof }
 import scala.reflect.api.JavaUniverse
 import scala.reflect.runtime.universe.TypeTag
 
-class GpuAggregationPartition[T <: Product: TypeTag](context: OpenCLContext,
+class GpuAggregationPartition[T <: Product: TypeTag](context: OpenCLContext, parentPartition: GpuPartition[T],
   groupByColumnIndexes: Array[Int], aggregations: Array[AggregationOperation.Value], capacity: Int)
     extends GpuPartition[T](context, capacity) {
-  //
-  def aggregate(): Unit = {
+
+  def aggregate(iterator: Iterator[T]): Unit = {
 
     def align(offset: Int): Int = {
       offset + (if (offset % 4 == 0) 0 else 4 - (offset % 4))
     }
 
-    val cpuOffsets: Array[Int] = columnTypes.map(baseSize(_)).scanLeft(0)(
+    val tupleCount = parentPartition.size
+
+    val cpuOffsets: Array[Int] = columnTypes.map(baseSize(_) * tupleCount).scanLeft(0)(
       {
         case (sum: Int, x: Int) => align(x) + sum
       }).toArray[Int]
 
     val totalSize = cpuOffsets.last
 
+
     val gpuContent = createReadBuffer[Byte](totalSize) // [Byte] because everything is in bytes
 
     var offsetIndex = 0
+    
+    
+    parentPartition.columnTypes.zipWithIndex.foreach { case (columnType, columnIndex) =>
+      implicit val columnTypeTag = javaTypeToTypeTag(columnType)
+      val column = parentPartition.getColumn(columnIndex)(columnTypeTag)
+      println(f"columnType($columnIndex) = $columnType")
+      println(column.array.asInstanceOf[Array[_]].mkString(","))
 
-    val tupleCount = this.size // TODO this.size or parent.size?
-    intData.foreach { column =>
-      hostToDeviceCopy[Byte](column, gpuContent, tupleCount , cpuOffsets(offsetIndex))
+      hostToDeviceCopy[Byte](column, gpuContent, tupleCount * baseSize(columnType), cpuOffsets(offsetIndex))
       offsetIndex += 1
     }
-
-    longData.foreach { column =>
-      hostToDeviceCopy[Long](column, gpuContent, tupleCount, cpuOffsets(offsetIndex))
-      offsetIndex += 1
-    }
-
-    floatData.foreach { column =>
-      hostToDeviceCopy[Float](column, gpuContent, tupleCount, cpuOffsets(offsetIndex))
-      offsetIndex += 1
-    }
-
-    doubleData.foreach { column =>
-      hostToDeviceCopy[Double](column, gpuContent, tupleCount, cpuOffsets(offsetIndex))
-      offsetIndex += 1
-    }
-
-    booleanData.foreach { column =>
-      hostToDeviceCopy[Boolean](column, gpuContent, tupleCount, cpuOffsets(offsetIndex))
-      offsetIndex += 1
-    }
-
-    charData.foreach { column =>
-      hostToDeviceCopy[Char](column, gpuContent, tupleCount, cpuOffsets(offsetIndex))
-      offsetIndex += 1
-    }
-
-    stringData.foreach { column =>
-      hostToDeviceCopy[Char](column, gpuContent, tupleCount * MAX_STRING_SIZE, cpuOffsets(offsetIndex))
-      offsetIndex += 1
-    }
+    
+    val gpuContentResults = new Array[Int](totalSize)
+    deviceToHostCopy[Byte](gpuContent, Pointer.to(gpuContentResults), totalSize, 0)
+    printf("gpuContentResults = %s\n", gpuContentResults.mkString(","))
+    
 
     val gpuOffsets = createReadBuffer[Int](cpuOffsets.length)
     hostToDeviceCopy[Int](pointer(cpuOffsets), gpuOffsets, cpuOffsets.length)
@@ -77,7 +60,7 @@ class GpuAggregationPartition[T <: Product: TypeTag](context: OpenCLContext,
 
     hostToDeviceCopy[Int](pointer(groupBySize), gpuGbSize, groupByColumnIndexes.length)
 
-    val gpuGbKey = createReadWriteBuffer[Int](this.size) //TODO  this.size or parent.size
+    val gpuGbKey = createReadWriteBuffer[Int](parentPartition.size) 
 
     val gpuGbIndex = createReadBuffer[Int](groupByColumnIndexes.length)
     hostToDeviceCopy[Int](pointer(groupByColumnIndexes), gpuGbIndex, groupByColumnIndexes.length)
@@ -94,8 +77,7 @@ class GpuAggregationPartition[T <: Product: TypeTag](context: OpenCLContext,
     clEnqueueNDRangeKernel(context.queue, memSetKernel, 1, null, global_work_size,
       local_work_size, 0, null, null)
 
-    val buildGroupByKeyKernel = clCreateKernel(context.program,
-      "build_groupby_key", null)
+    val buildGroupByKeyKernel = clCreateKernel(context.program, "build_groupby_key", null)
     clSetKernelArg(buildGroupByKeyKernel, 0, Sizeof.cl_mem, Pointer.to(gpuContent))
     clSetKernelArg(buildGroupByKeyKernel, 1, Sizeof.cl_mem, Pointer.to(gpuOffsets))
     clSetKernelArg(buildGroupByKeyKernel, 2, Sizeof.cl_int,
@@ -111,6 +93,14 @@ class GpuAggregationPartition[T <: Product: TypeTag](context: OpenCLContext,
     clEnqueueNDRangeKernel(context.queue, buildGroupByKeyKernel, 1, null, global_work_size,
       local_work_size, 0, null, null)
 
+    val gpu_hashNumResults = new Array[Int](HASH_SIZE)
+    deviceToHostCopy[Int](gpu_hashNum, Pointer.to(gpu_hashNumResults), HASH_SIZE, 0)
+    printf("gpu_hashNumResults = %s\n", gpu_hashNumResults.zipWithIndex.filter(_._1 != 0).mkString(","))
+
+    val gpuGbKeyResults = new Array[Int](parentPartition.size)
+    deviceToHostCopy[Int](gpuGbKey, Pointer.to(gpuGbKeyResults), parentPartition.size, 0)
+    printf("gpuGbKeyResults = %s\n", gpuGbKeyResults.take(100).mkString(","))
+    
     clReleaseMemObject(gpuGbType)
     clReleaseMemObject(gpuGbSize)
     clReleaseMemObject(gpuGbIndex)
@@ -128,6 +118,8 @@ class GpuAggregationPartition[T <: Product: TypeTag](context: OpenCLContext,
     val gbCount = Array[Int](1)
 
     deviceToHostCopy[Int](gpuGbCount, pointer(gbCount), 1)
+    
+    println("Groupby count = %s".format(gbCount.mkString(",")) )
 
     val gpu_psum = createReadWriteBuffer[Int](HASH_SIZE)
 
@@ -138,8 +130,10 @@ class GpuAggregationPartition[T <: Product: TypeTag](context: OpenCLContext,
   }
 
   override def fill(iter: Iterator[T]): Unit = {
-    super.fill(iter)
-    this.aggregate()
+    parentPartition.inferBestWorkGroupSize
+    this.globalSize = parentPartition.globalSize
+    this.localSize = parentPartition.localSize
+    this.aggregate(iter)
   }
 }
 
