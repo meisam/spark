8984db9c7ea48cc688fdd3e381fadc340462334e
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-12-02 10:07:31 -0500

    Adding a few more test cases to gpu aggregation
    #30: port aggregations from opencl/cuda implementation
    
    Task-Url: http://github.com/meisam/spark/issues/issue/30

--- core/src/test/scala/org/apache/spark/gpu/GpuAggregationPartitionSuit.scala
+++ core/src/test/scala/org/apache/spark/gpu/GpuAggregationPartitionSuit.scala
@@ -39,8 +39,8 @@ class GpuAggregationPartitionSuit extends FunSuite with SharedSparkContext {
     openCLContext.initOpenCL("/org/apache/spark/gpu/kernel.cl")
   }
 
-  test("Aggregation simpl(Int, Int)  match test") {
-    val testData: IndexedSeq[(Int, Int)] = Array((11, 0), (11, 1), (11, 2), (12, 6), (12, 5))
+  test("Aggregation simple(Int, Int)  test") {
+    val testData: IndexedSeq[(Int, Int)] = Array((11, 3), (11, 1), (11, 4), (12, 6), (12, 5))
 
     val partition = new GpuPartition[(Int, Int)](openCLContext, DEFAULT_CAPACITY)
     
@@ -57,6 +57,50 @@ class GpuAggregationPartitionSuit extends FunSuite with SharedSparkContext {
     expectedData.zipWithIndex.foreach { case ((gbVal, aggValue), index) =>
       assert(aggregationPartition.intData(0).get(index) === gbVal, "values do not match")
       assert(aggregationPartition.intData(1).get(index) === aggValue, "values do not match")
+    case _ => fail("We should not be here")
+    }
+  }
+
+  test("Aggregation simple(Int, Int)  2 test") {
+    val testData: IndexedSeq[(Int, Int)] = Array((111, 3), (111, 1), (111, 4), (112, 6), (112, 5))
+
+    val partition = new GpuPartition[(Int, Int)](openCLContext, DEFAULT_CAPACITY)
+    
+    partition.fill(testData.toIterator)
+    
+    val aggregationPartition = new GpuAggregationPartition[(Int, Int)](openCLContext, partition
+      , Array(0), Array(AggregationOperation.max), DEFAULT_CAPACITY)
+
+    aggregationPartition.fill(testData.toIterator)
+    val expectedData = Array((11, 2), (12, 6))
+
+    assert(aggregationPartition.size === expectedData.length)
+
+    expectedData.zipWithIndex.foreach { case ((gbVal, aggValue), index) =>
+      assert(aggregationPartition.intData(0).get(index) === gbVal, "values do not match")
+      assert(aggregationPartition.intData(1).get(index) === aggValue, "values do not match")
+    case _ => fail("We should not be here")
+    }
+  }
+
+  test("Aggregation simple(Int, Long)  match test") {
+    val testData: IndexedSeq[(Int, Long)] = Array((11, 3L), (11, 1L), (11, 4L), (12, 6L), (12, 5L))
+
+    val partition = new GpuPartition[(Int, Long)](openCLContext, DEFAULT_CAPACITY)
+    
+    partition.fill(testData.toIterator)
+    
+    val aggregationPartition = new GpuAggregationPartition[(Int, Long)](openCLContext, partition
+      , Array(0), Array(AggregationOperation.max), DEFAULT_CAPACITY)
+
+    aggregationPartition.fill(testData.toIterator)
+    val expectedData = Array((11, 4L), (12, 6L))
+
+    assert(aggregationPartition.size === expectedData.length)
+
+    expectedData.zipWithIndex.foreach { case ((gbVal, aggValue), index) =>
+      assert(aggregationPartition.intData(0).get(index) === gbVal, "values do not match")
+      assert(aggregationPartition.longData(0).get(index) === aggValue, "values do not match")
     case _ => fail("We should not be here")
     }
   }
