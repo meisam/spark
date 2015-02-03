4516041834e6c01d3137e0f47f88429814c9593b
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2015-02-03 11:01:30 -0500

    Fixing GpuFilteredPartitionIterator
    #53: GpuFiltredRDD does not work properly 
    
    Task-Url: http://github.com/meisam/spark/issues/issue/53

--- core/src/main/scala/org/apache/spark/rdd/GpuFilteredPartitionIterator.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuFilteredPartitionIterator.scala
@@ -9,7 +9,32 @@ class GpuFilteredPartitionIterator[T <: Product : TypeTag, V: TypeTag]
  colIndex: Int, operation: ComparisonOperation.Value, value: V, chunkCapacity: Int)
   extends Serializable with Iterator[GpuPartition[T]] {
 
-  override def hasNext: Boolean = itr.hasNext
+ override def hasNext: Boolean = {
+  parent.hasNext
+ }
+
+ protected var currentPosition: Int = -1
+
+ @transient protected val context: OpenCLContext = new OpenCLContext
+
+ context.initOpenCL("/org/apache/spark/gpu/kernel.cl")
+
+ protected val currentPartition = new GpuFilteredPartition[T, V](
+  context, currentPosition, colIndex, operation, value, chunkCapacity)
+
+ override def next(): GpuPartition[T] = {
+  guaranteeFill
+   currentPosition += 1
+   new GpuFilteredPartition[T, V](
+     context, currentPosition, colIndex, operation, value, chunkCapacity)
+ }
+
+ def guaranteeFill {
+   val parentPartition: GpuPartition[T] = parent.next()
+   currentPartition.filter(parentPartition)
+   currentPosition = +1
+ }
+
 }
 
 
