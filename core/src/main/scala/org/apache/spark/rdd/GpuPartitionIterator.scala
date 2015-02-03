43b7cbc160801e8f298f2251f2cf1597707b7ba2
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2015-02-03 11:03:34 -0500

    Removing the redundant logic from GpuPartitionIterator
    #53: GpuFiltredRDD does not work properly
    
    Task-Url: http://github.com/meisam/spark/issues/issue/53

--- core/src/main/scala/org/apache/spark/rdd/GpuPartitionIterator.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuPartitionIterator.scala
@@ -1,38 +1,16 @@
 package org.apache.spark.rdd
 
-import org.apache.spark.scheduler.OpenCLContext
-
-import scala.reflect.ClassTag
-import scala.reflect.runtime.universe.{TypeTag, typeOf}
-import scala.reflect.runtime.{universe => ru}
+import scala.reflect.runtime.universe.TypeTag
 
 class GpuPartitionIterator[T <: Product : TypeTag]
-(itr: Iterator[T], chunkCapacity: Int = 1 << 20)
-  extends Serializable with Iterator[T] {
+(itr: Iterator[GpuPartition[T]], chunkCapacity: Int = 1 << 20)
+  extends Serializable with Iterator[GpuPartition[T]] {
 
   override def hasNext: Boolean = {
-    currentPosition < currentPartition.size || itr.hasNext
+    itr.hasNext
   }
 
-  protected var currentPosition: Int = -1
-
-  @transient protected val context: OpenCLContext = new OpenCLContext
-
-  context.initOpenCL("/org/apache/spark/gpu/kernel.cl")
-
-  protected val currentPartition: GpuPartition[T] = new GpuPartition[T](context, chunkCapacity)
-
-  override def next(): T = {
-    guaranteeFill
-    val t: T = currentPartition(currentPosition)
-    currentPosition += 1
-    t
-  }
-
-  def guaranteeFill {
-    if (currentPosition >= currentPartition.size || currentPosition < 0) {
-      currentPartition.fill(itr)
-      currentPosition = 0
-    }
+  override def next(): GpuPartition[T] = {
+    itr.next
   }
 }
