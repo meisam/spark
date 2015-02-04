da0e1c68d378db4dabd2010e6a4ba717289a5c78
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2015-02-04 17:48:00 -0500

    #59: If data does not fill into a GpuPartition, GpuRdd.compute should return a sequence of Partitions.
    Task-Url: http://github.com/meisam/spark/issues/issue/59

--- core/src/main/scala/org/apache/spark/rdd/GpuPartitionIterator.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuPartitionIterator.scala
@@ -3,14 +3,16 @@ package org.apache.spark.rdd
 import scala.reflect.runtime.universe.TypeTag
 
 class GpuPartitionIterator[T <: Product : TypeTag]
-(itr: Iterator[GpuPartition[T]], chunkCapacity: Int = 1 << 20)
+(itr: Iterator[T], capacity: Int = 1 << 20)
   extends Serializable with Iterator[GpuPartition[T]] {
 
-  override def hasNext: Boolean = {
-    itr.hasNext
+  override def next(): GpuPartition[T] = {
+    val nextDataChunk = itr.take(capacity)
+    val optimalCapacity = Math.min(nextDataChunk.length, capacity)
+    val currentPartition = new GpuPartition[T](null, optimalCapacity)
+    currentPartition.fill(nextDataChunk)
+    currentPartition
   }
 
-  override def next(): GpuPartition[T] = {
-    itr.next
-  }
+  override def hasNext: Boolean = itr.hasNext
 }
