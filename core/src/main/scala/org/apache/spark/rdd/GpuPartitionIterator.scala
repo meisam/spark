377b9d3c994e0509a81a18b2cac074c99399cd88
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2015-02-06 16:14:16 -0500

    #59: If data does not fill into a GpuPartition, GpuRdd.compute should return a sequence of Partitions.
    Task-Url: http://github.com/meisam/spark/issues/issue/59

--- core/src/main/scala/org/apache/spark/rdd/GpuPartitionIterator.scala
+++ core/src/main/scala/org/apache/spark/rdd/GpuPartitionIterator.scala
@@ -1,16 +1,31 @@
 package org.apache.spark.rdd
 
-import scala.reflect.runtime.universe.TypeTag
+import org.apache.spark.Logging
+
+import scala.reflect.ClassTag
+import scala.reflect.runtime.universe.{TypeTag, typeOf}
+import scala.reflect.runtime.{universe => ru}
 
 class GpuPartitionIterator[T <: Product : TypeTag]
 (itr: Iterator[T], capacity: Int = 1 << 20)
-  extends Serializable with Iterator[GpuPartition[T]] {
+  extends Serializable with Iterator[GpuPartition[T]] with Logging{
+
+  implicit val ct = ClassTag[T](ru.runtimeMirror(getClass.getClassLoader).runtimeClass(typeOf[T]))
+  val data: Array[T] = itr.toArray(ct)
+  var currentPosition = 0
 
   override def next(): GpuPartition[T] = {
+    if (currentPosition >= data.length) {
+      throw new ArrayIndexOutOfBoundsException(f"${currentPosition} >= ${data.length}")
+    }
+    val slice = data.slice(currentPosition, currentPosition + capacity)
+    currentPosition += slice.length
     val currentPartition = new GpuPartition[T](null, capacity)
-    currentPartition.fill(itr)
+    currentPartition.fill(slice.toIterator)
     currentPartition
   }
 
-  override def hasNext: Boolean = itr.hasNext
+  override def hasNext: Boolean = {
+    currentPosition < data.length
+  }
 }
