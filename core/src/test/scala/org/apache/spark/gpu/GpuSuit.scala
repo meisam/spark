3d55ea65645296ae00803ee7f3abc286d7876a06
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2015-02-24 10:39:25 -0500

    cleaning up test cases #51: Remove the unnecessary test cases
    Task-Url: http://github.com/meisam/spark/issues/issue/51

--- core/src/test/scala/org/apache/spark/gpu/GpuSuit.scala
+++ core/src/test/scala/org/apache/spark/gpu/GpuSuit.scala
@@ -3,6 +3,7 @@ package org.apache.spark.gpu
 
 import org.apache.spark.SharedSparkContext
 import org.apache.spark.rdd.GpuPartition
+import org.apache.spark.scheduler.OpenCLContextSingletone
 import org.scalatest.FunSuite
 
 import scala.language.existentials
@@ -14,7 +15,12 @@ import scala.reflect.runtime.universe.TypeTag
  */
 class GpuSuit extends FunSuite with SharedSparkContext {
 
-  private def flattenResults[T <: Product : TypeTag : ClassTag](collectedPartitions: Array[GpuPartition[T]]): Array[T] = {
+  val DEFAULT_CAPACITY = (1 << 10)
+
+  val openCLContext = OpenCLContextSingletone.openClContext
+
+  protected def flattenResults[T <: Product : TypeTag : ClassTag](collectedPartitions:
+    Array[GpuPartition[T]]): Array[T] = {
 
     val collectedData: Array[T] = collectedPartitions.map { p =>
       (0 until p.size).map(i => p(i))
@@ -24,13 +30,14 @@ class GpuSuit extends FunSuite with SharedSparkContext {
 
   def validateResults[T <: Product : TypeTag : ClassTag](expectedData: Array[T], collectedPartitions: Array[GpuPartition[T]]):
   Unit = {
+    val totalResult = collectedPartitions.map(p => p.size).reduce(_ + _)
+    assert(totalResult === expectedData.length)
+
     val collectedData = flattenResults[T](collectedPartitions)
     expectedData.zip(collectedData).foreach {
       case (vt, vc) =>
         assert(vt === vc)
     }
 
-    val totalResult = collectedPartitions.map(p => p.size).reduce(_ + _)
-    assert(totalResult === expectedData.length)
   }
 }
